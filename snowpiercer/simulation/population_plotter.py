3d6f71f973242e82a3425d56ec9b9aca1997ce33
uzanto <git@uzanto.me>
2023-08-22 10:45:22 +0200

    add more metrics to data_log and plotting

--- snowpiercer/simulation/population_plotter.py
+++ snowpiercer/simulation/population_plotter.py
@@ -1,15 +1,22 @@
 import numpy as np
 import numpy.typing as npt
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
 class PopulationPlotter:
-
-    def plot(self, population_size_log : npt.NDArray['int']):
-        plt.plot(population_size_log)
-        plt.title("Population Size")
-        plt.xlabel("Time")
-        plt.ylabel("Individuals")
-        plt.grid(True)
-        #plt.savefig("docs/plots/population_size.png")
+    
+    def plot(self, df: pd.DataFrame, population_size: bool = True, born: bool = True, died: bool = True):
+        
+        plt.xlabel('Time')
+        plt.ylabel('Number of individuals')
+        
+        if population_size:
+            plt.plot(df['time'], df['population_size'], label='Population size')
+        if born:
+            plt.plot(df['time'], df['born'], label='Born rate')
+        if died:
+            plt.plot(df['time'], df['died'], label='Died rate')
+        
+        plt.legend()
         plt.show()
--- snowpiercer/simulation/simulation.py
+++ snowpiercer/simulation/simulation.py
@@ -1,31 +1,40 @@
 import numpy as np
 import numpy.typing as npt
+import pandas as pd
 
 from snowpiercer.world.world import World
 
 
 class Simulation:
     number_of_timesteps: int = None
-
-    world: World = None
-    population_size_log : npt.NDArray['int'] = None
-
+    world: World = None    
+    data: pd.DataFrame = None
+    
     def __init__(self, number_of_timesteps: int, world: World):
         self.number_of_timesteps = number_of_timesteps
         self.world = world
-        self.population_size_log = np.empty(self.number_of_timesteps, dtype='int')
-
+        self.data = pd.DataFrame(columns=['time', 'population_size', 'born', 'died'])
+        
     def run(self):
-        self.population_size_log = self.run_experiment()
+        self.data = self.run_experiment()
 
-    def run_experiment(self) -> npt.NDArray['int']:
-        population_size_log: npt.NDArray['int'] = np.empty(self.number_of_timesteps, dtype='int')
+    def run_experiment(self) -> pd.DataFrame:        
+        data = {'time': np.zeros(self.number_of_timesteps), 'population_size': np.zeros(self.number_of_timesteps), 'born': np.zeros(self.number_of_timesteps), 'died': np.zeros(self.number_of_timesteps)}
+        
         for i in range(self.number_of_timesteps):
             population_size: int = self.world.get_population_size()
-            population_size_log[i] = population_size
+            born: int = self.world.get_born_count()
+            died: int = self.world.get_died_count()
+            
+            data['time'][i] = i
+            data['population_size'][i] = population_size
+            data['born'][i] = born
+            data['died'][i] = died
+            
             self.world.next_time_step()
-        return population_size_log
 
+        df = pd.DataFrame.from_dict(data)
+        return df
 
-    def get_population_size_log(self):
-        return self.population_size_log
+    def get_data_log(self) -> pd.DataFrame:
+        return self.data
