6ce698f34821ea467d3f8ae5374592d49d86668e
Timo Weberruß <timo.weberruss@student.kit.edu>
2023-08-23 09:13:44 +0200

    Remove wood printing

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from snowpiercer.world import World
 
 # main script
 if __name__ == '__main__':
-    simulation: Simulation = Simulation(number_of_timesteps=200, world=World.create_appletree_world_with_few_lumberjacks())
+    simulation: Simulation = Simulation(number_of_timesteps=200, world=World.create_simple_mixed_world())
     simulation.run()
     PopulationPlotter().plot(simulation.get_data_log())
-    time.sleep(5)
-    PopulationPlotter().plot_wood_resources(simulation.get_data_log())
